972ee618400e0505c9ee76c557f7330a3bb792c9
Muhammad Faiz Asy Syauqi <m.faizasysyauqi@gmail.com>
2026-09-25 16:31:01 +0700

    fix: allow direct access to platform setup layout without redirect

--- packages/web/src/app/components/platform-layout.tsx
+++ packages/web/src/app/components/platform-layout.tsx
@@ -13,30 +13,25 @@ import { PlatformSidebar } from './sidebar/platform';
 
 export function PlatformLayout({ children }: { children: React.ReactNode }) {
   const { data: edition } = flagsHooks.useFlag<ApEdition>(ApFlagId.EDITION);
-  const showPlatformAdminDashboard = useIsPlatformAdmin();
 
   return (
     <AllowOnlyLoggedInUserOnlyGuard>
       <GlobalSearchProvider>
-        {showPlatformAdminDashboard ? (
-          <SidebarProvider open={true}>
-            <PlatformSidebar />
-            <SidebarInset className="flex flex-col h-full overflow-hidden bg-sidebar">
-              <div className="flex-1 flex flex-col p-2 pt-3 pb-3 overflow-hidden">
-                <div
-                  id="dashboard-content-container"
-                  className="relative flex flex-col h-full bg-background rounded-xl shadow-[2px_0px_4px_-2px_rgba(0,0,0,0.05),0px_2px_4px_-2px_rgba(0,0,0,0.05)] border overflow-clip"
-                >
-                  <div className="flex flex-col flex-1 overflow-auto">
-                    {children}
-                  </div>
+        <SidebarProvider open={true}>
+          <PlatformSidebar />
+          <SidebarInset className="flex flex-col h-full overflow-hidden bg-sidebar">
+            <div className="flex-1 flex flex-col p-2 pt-3 pb-3 overflow-hidden">
+              <div
+                id="dashboard-content-container"
+                className="relative flex flex-col h-full bg-background rounded-xl shadow-[2px_0px_4px_-2px_rgba(0,0,0,0.05),0px_2px_4px_-2px_rgba(0,0,0,0.05)] border overflow-clip"
+              >
+                <div className="flex flex-col flex-1 overflow-auto">
+                  {children}
                 </div>
               </div>
-            </SidebarInset>
-          </SidebarProvider>
-        ) : (
-          <Navigate to="/" />
-        )}
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
         {edition !== ApEdition.COMMUNITY && <ManagePlanDialog />}
       </GlobalSearchProvider>
     </AllowOnlyLoggedInUserOnlyGuard>
